7a4153aed077b7a4fb679177f5053e353fe58a09
siwon <muncool39@gmail.com>
2024-01-06 22:05:31 +0900

    [FEAT] 좋아요 엔티티 설계
    #1

---
+++ src/main/java/com/triprecord/triprecord/like/Like.java
@@ -0,0 +1,42 @@
+package com.triprecord.triprecord.like;
+
+
+import com.triprecord.triprecord.record.Record;
+import com.triprecord.triprecord.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "`like`")
+@NoArgsConstructor
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long likeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "record_id")
+    private Record likedRecord;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User likedUser;
+
+    @Builder
+    public Like(Record recordId, User userId) {
+        this.likedRecord = recordId;
+        this.likedUser = userId;
+    }
+
+}
