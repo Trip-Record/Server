1ddafdf893c1244a4816afff45ae1465e884c65f
siwon <muncool39@gmail.com>
2024-01-12 03:10:16 +0900

    [FEAT] 세부 일정 엔티티 설계
    #7

---
+++ src/main/java/com/triprecord/triprecord/scheduledetail/ScheduleDetail.java
@@ -0,0 +1,41 @@
+package com.triprecord.triprecord.scheduledetail;
+
+import com.triprecord.triprecord.schedule.Schedule;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ScheduleDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long scheduleDetailId;
+
+    private LocalDate scheduleDetailDate;
+
+    private String scheduleContent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")
+    private Schedule linkedSchedule;
+
+
+    @Builder
+    public ScheduleDetail(LocalDate localDate, String content, Schedule schedule) {
+        this.scheduleDetailDate = localDate;
+        this.scheduleContent = content;
+        this.linkedSchedule = schedule;
+    }
+}
