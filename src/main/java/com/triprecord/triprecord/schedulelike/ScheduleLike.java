f21da60564782c19134f854e375de88fbad2a44d
siwon <muncool39@gmail.com>
2024-01-12 03:10:58 +0900

    [FEAT] 일정 코멘트 좋아요 엔티티 설계
     #7

---
+++ src/main/java/com/triprecord/triprecord/schedulelike/ScheduleLike.java
@@ -0,0 +1,41 @@
+package com.triprecord.triprecord.schedulelike;
+
+
+import com.triprecord.triprecord.schedule.Schedule;
+import com.triprecord.triprecord.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ScheduleLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long scheduleLikeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")
+    private Schedule likedSchedule;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User likedUser;
+
+
+    @Builder
+    public ScheduleLike(Schedule schedule, User user) {
+        this.likedSchedule = schedule;
+        this.likedUser = user;
+    }
+}
