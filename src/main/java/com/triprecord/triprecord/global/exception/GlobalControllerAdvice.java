9837551bf64c275bef6f4e0e4b10fda3ea0ca59a
siwon <muncool39@gmail.com>
2024-01-14 21:52:00 +0900

    [FEAT] 공통 예외 처리 구현
    -TripRecordException
    -ErrorCode

--- src/main/java/com/triprecord/triprecord/global/exception/GlobalControllerAdvice.java
+++ src/main/java/com/triprecord/triprecord/global/exception/GlobalControllerAdvice.java
@@ -13,6 +13,12 @@ public class GlobalControllerAdvice {
 
     private final String ERROR_LOG = "[ERROR] %s %s";
 
+    @ExceptionHandler(TripRecordException.class)
+    public ResponseEntity<ExceptionDTO> applicationException(final TripRecordException e){
+        log.error(String.format(ERROR_LOG, e.getHttpStatus(), e.getMessage()));
+        return ResponseEntity.status(e.getHttpStatus()).body(new ExceptionDTO(e.getMessage()));
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionDTO> methodArgumentNotValidException(final MethodArgumentNotValidException e){
         log.error(String.format(ERROR_LOG, e.getParameter(), "객체검증에러"));
