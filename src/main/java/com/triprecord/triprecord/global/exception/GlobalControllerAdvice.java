bec33a07b11cc9a836ccc94be2459ce4078c3188
siwon <muncool39@gmail.com>
2024-01-14 21:47:00 +0900

    [FEAT] 객체 검증 에러 처리

---
+++ src/main/java/com/triprecord/triprecord/global/exception/GlobalControllerAdvice.java
@@ -0,0 +1,24 @@
+package com.triprecord.triprecord.global.exception;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalControllerAdvice {
+
+    private final String ERROR_LOG = "[ERROR] %s %s";
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionDTO> methodArgumentNotValidException(final MethodArgumentNotValidException e){
+        log.error(String.format(ERROR_LOG, e.getParameter(), "객체검증에러"));
+        return ResponseEntity.ok(new ExceptionDTO("필요한 데이터가 모두 입력되지 않았습니다."));
+    }
+
+    record ExceptionDTO(String message){
+    }
+}
