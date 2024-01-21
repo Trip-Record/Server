292dd7d71a939c8fe958bdeec976dd6dab7f7cd7
siwon <muncool39@gmail.com>
2024-01-21 14:51:49 +0900

    [FEAT] 기록 생성을 위한 API
    -POST /record

---
+++ src/main/java/com/triprecord/triprecord/record/controller/RecordController.java
@@ -0,0 +1,29 @@
+package com.triprecord.triprecord.record.controller;
+
+
+import com.triprecord.triprecord.record.controller.request.RecordCreateRequest;
+import com.triprecord.triprecord.record.service.RecordService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/record")
+public class RecordController {
+
+    private final RecordService recordService;
+
+    @PostMapping()
+    public ResponseEntity<String> createRecord(Authentication authentication, @Valid RecordCreateRequest request){
+        recordService.createRecord(Long.valueOf(authentication.getName()), request);
+        return ResponseEntity.status(HttpStatus.CREATED).body("기록 생성에 성공했습니다.");
+    }
+
+}
