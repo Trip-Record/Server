227406485355b02e4863dddd15c62f5935b5b7ff
시원 <105481797+muncool39@users.noreply.github.com>
2024-01-26 23:44:35 +0900

    [FEAT] 기록 삭제 API (#41)
    * [FEAT] S3의 기록 이미지 삭제
    * [FEAT] 기록 삭제 API

--- src/main/java/com/triprecord/triprecord/record/service/RecordImageService.java
+++ src/main/java/com/triprecord/triprecord/record/service/RecordImageService.java
@@ -5,12 +5,15 @@ import com.triprecord.triprecord.record.entity.Record;
 import com.triprecord.triprecord.record.entity.RecordImage;
 import com.triprecord.triprecord.record.repository.RecordImageRepository;
 import java.net.URL;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RecordImageService {
@@ -30,6 +33,15 @@ public class RecordImageService {
         recordImageRepository.save(recordImage);
     }
 
+    public void deleteS3RecordImage(Record record){
+        List<RecordImage> recordImages = recordImageRepository.findAllByLinkedRecord(record);
+        if(recordImages.isEmpty()) return;
+        String basicS3Path = s3Service.getFileURLFromS3(null).toString();
+        for (RecordImage image : recordImages){
+            String imageName = image.getRecordImgUrl().substring(basicS3Path.length());
+            s3Service.deleteFileFromS3(imageName);
+        }
+    }
 
     private String getNameAddRandomUUID(String originName) {
         String randomUUID = UUID.randomUUID().toString();
