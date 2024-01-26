4278d735de42dde0c02f926cbff86f66fb1211b2
gitseoyeon <tjdus553926@gmail.com>
2024-01-27 00:25:23 +0900

    [FIX] 충돌관련 에러 해결

--- src/main/java/com/triprecord/triprecord/user/dto/request/UserCreateRequest.java
+++ src/main/java/com/triprecord/triprecord/user/dto/request/UserCreateRequest.java
@@ -1,9 +1,4 @@
-<<<<<<<< HEAD:src/main/java/com/triprecord/triprecord/user/dto/request/UserCreateRequest.java
 package com.triprecord.triprecord.user.dto.request;
-========
-package com.triprecord.triprecord.user.controller.dto.request;
->>>>>>>> a4a5d558dceddcaf80a6e4b81c685b280443947c:src/main/java/com/triprecord/triprecord/user/controller/dto/request/UserCreateRequest.java
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 
