c9aed75249e702f7cff00d2d491d07c6f81369c4
siwon <muncool39@gmail.com>
2024-01-26 21:17:42 +0900

    [FEAT] 기록 수정 API

--- src/main/java/com/triprecord/triprecord/record/controller/RecordController.java
+++ src/main/java/com/triprecord/triprecord/record/controller/RecordController.java
@@ -3,6 +3,7 @@ package com.triprecord.triprecord.record.controller;
 
 import com.triprecord.triprecord.global.util.ResponseMessage;
 import com.triprecord.triprecord.record.controller.request.RecordCreateRequest;
+import com.triprecord.triprecord.record.controller.request.RecordModifyRequest;
 import com.triprecord.triprecord.record.service.RecordService;
 import jakarta.validation.Valid;
 import java.util.Map;
@@ -10,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +31,13 @@ public class RecordController {
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseMessage("기록 생성에 성공했습니다."));
     }
 
+    @PatchMapping("/{recordId}")
+    public ResponseEntity<ResponseMessage> modifyRecord(Authentication authentication,
+                                                        @PathVariable Long recordId,
+                                                        @Valid RecordModifyRequest request){
+        recordService.modifyRecord(Long.valueOf(authentication.getName()), recordId, request);
+        return ResponseEntity.ok(new ResponseMessage("기록 수정에 성공했습니다."));
+    }
 
 
 }
