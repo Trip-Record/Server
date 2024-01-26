7940b878ee3bd5dc591377e00837b4da96d31a04
siwon <muncool39@gmail.com>
2024-01-27 03:57:33 +0900

    [ADD] 해당 레코드의 좋아요 수를 갖는 서비스

--- src/main/java/com/triprecord/triprecord/record/repository/RecordLikeRepository.java
+++ src/main/java/com/triprecord/triprecord/record/repository/RecordLikeRepository.java
@@ -1,5 +1,6 @@
 package com.triprecord.triprecord.record.repository;
 
+import com.triprecord.triprecord.record.entity.Record;
 import com.triprecord.triprecord.record.entity.RecordLike;
 import com.triprecord.triprecord.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RecordLikeRepository extends JpaRepository<RecordLike, Long> {
 
+    Long countByLikedRecord(Record record);
     Long countRecordLikeByLikedUser(User user);
 }
