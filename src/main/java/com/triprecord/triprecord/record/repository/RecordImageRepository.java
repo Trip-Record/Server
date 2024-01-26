dfa390275b795431898c0e2fe9534f0552364798
siwon <muncool39@gmail.com>
2024-01-26 12:35:09 +0900

    [FEAT] S3의 기록 이미지 삭제

--- src/main/java/com/triprecord/triprecord/record/repository/RecordImageRepository.java
+++ src/main/java/com/triprecord/triprecord/record/repository/RecordImageRepository.java
@@ -1,9 +1,14 @@
 package com.triprecord.triprecord.record.repository;
 
+import com.triprecord.triprecord.record.entity.Record;
 import com.triprecord.triprecord.record.entity.RecordImage;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecordImageRepository extends JpaRepository<RecordImage, Long> {
+
+    List<RecordImage> findAllByLinkedRecord(Record linkedRecord);
 }
