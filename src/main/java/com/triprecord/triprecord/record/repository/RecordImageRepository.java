277d12d357592fb1ca555987d48c42b46e64e0cb
siwon <muncool39@gmail.com>
2024-01-27 01:36:30 +0900

    [MERGE] 충돌 해결

--- src/main/java/com/triprecord/triprecord/record/repository/RecordImageRepository.java
+++ src/main/java/com/triprecord/triprecord/record/repository/RecordImageRepository.java
@@ -10,10 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RecordImageRepository extends JpaRepository<RecordImage, Long> {
 
-<<<<<<< HEAD
     long countByLinkedRecord(Record linkedRecord);
     Optional<RecordImage> findAllByLinkedRecordAndRecordImgUrl(Record linkedRecord, String recordImgUrl);
-=======
     List<RecordImage> findAllByLinkedRecord(Record linkedRecord);
->>>>>>> 227406485355b02e4863dddd15c62f5935b5b7ff
 }
