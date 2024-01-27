b0d3beabdaf28985d5c09babfc07be6671d465be
siwon <muncool39@gmail.com>
2024-01-27 15:43:31 +0900

    [ADD] 좋아요 생성 서비스

--- src/main/java/com/triprecord/triprecord/record/repository/RecordLikeRepository.java
+++ src/main/java/com/triprecord/triprecord/record/repository/RecordLikeRepository.java
@@ -3,6 +3,7 @@ package com.triprecord.triprecord.record.repository;
 import com.triprecord.triprecord.record.entity.Record;
 import com.triprecord.triprecord.record.entity.RecordLike;
 import com.triprecord.triprecord.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,5 @@ public interface RecordLikeRepository extends JpaRepository<RecordLike, Long> {
 
     Long countByLikedRecord(Record record);
     Long countRecordLikeByLikedUser(User user);
+    Optional<RecordLike> findByLikedRecordAndLikedUser(Record record, User likedUser);
 }
