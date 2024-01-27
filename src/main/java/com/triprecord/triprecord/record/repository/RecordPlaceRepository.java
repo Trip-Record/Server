3113edc190f61caf72949e83c69c83c2a3d6d255
siwon <muncool39@gmail.com>
2024-01-27 14:13:44 +0900

    [ADD] 레코드 장소 정보 반환 서비스

--- src/main/java/com/triprecord/triprecord/record/repository/RecordPlaceRepository.java
+++ src/main/java/com/triprecord/triprecord/record/repository/RecordPlaceRepository.java
@@ -4,6 +4,7 @@ import com.triprecord.triprecord.location.entity.Place;
 import com.triprecord.triprecord.record.entity.Record;
 import com.triprecord.triprecord.record.entity.RecordPlace;
 import com.triprecord.triprecord.user.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,6 +15,10 @@ public interface RecordPlaceRepository extends JpaRepository<RecordPlace, Long> 
 
     long countByLinkedRecord(Record linkedRecord);
     void deleteByLinkedRecordAndRecordPlace(Record linkedRecord, Place recordPlace);
+
+    @Query("SELECT r.recordPlace FROM RecordPlace r WHERE r.linkedRecord = :record")
+    List<Place> findAllPlaceByLinkedRecord(Record record);
+
     @Query("select count(distinct rp.recordPlace) from RecordPlace rp where rp.linkedRecord.createdUser = :user")
     Long placeCount(@Param("user") User user);
 }
