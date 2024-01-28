9f8c323f9a19fcb489fd084a2f50153d44400264
gitseoyeon <tjdus553926@gmail.com>
2024-01-28 19:51:27 +0900

    [REFACTOR] Http 응답 방식 수정

--- src/main/java/com/triprecord/triprecord/user/controller/TripStyleController.java
+++ src/main/java/com/triprecord/triprecord/user/controller/TripStyleController.java
@@ -3,6 +3,7 @@ package com.triprecord.triprecord.user.controller;
 import com.triprecord.triprecord.user.dto.response.TripStyleInfo;
 import com.triprecord.triprecord.user.service.TripStyleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,6 @@ public class TripStyleController {
     private final TripStyleService tripStyleService;
     @PostMapping("/trip-styles")
     public ResponseEntity<List<TripStyleInfo>> tripStyleInfo(){
-        return ResponseEntity.ok().body(tripStyleService.getTripStyleInfo());
+        return ResponseEntity.status(HttpStatus.OK).body(tripStyleService.getTripStyleInfo());
     }
 }
