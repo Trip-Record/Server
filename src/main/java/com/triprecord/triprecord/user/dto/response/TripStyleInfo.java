5817ef5086c13997c3a92695899af852fcb235c6
gitseoyeon <tjdus553926@gmail.com>
2024-01-28 19:53:25 +0900

    [ADD] TripStyleInfo에 tripStyle id 추가

--- src/main/java/com/triprecord/triprecord/user/dto/response/TripStyleInfo.java
+++ src/main/java/com/triprecord/triprecord/user/dto/response/TripStyleInfo.java
@@ -3,12 +3,14 @@ package com.triprecord.triprecord.user.dto.response;
 import com.triprecord.triprecord.user.entity.TripStyle;
 
 public record TripStyleInfo(
+        Long tripStyleId,
         String tripStyleName,
         String tripStyleImg,
         String tripStyleDescription
 ) {
     public static TripStyleInfo of(TripStyle tripStyle){
         return new TripStyleInfo(
+                tripStyle.getTripStyleId(),
                 tripStyle.getTripStyleName(),
                 tripStyle.getTripStyleImg(),
                 tripStyle.getTripStyleDescription()
