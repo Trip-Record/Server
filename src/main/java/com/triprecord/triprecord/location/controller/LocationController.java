2f0e738e2da443253fb39d6dd65d5216771665c9
gitseoyeon <tjdus553926@gmail.com>
2024-01-31 22:27:37 +0900

    [REFACTOR] API 응답 리턴하는 코드 수정

--- src/main/java/com/triprecord/triprecord/location/controller/LocationController.java
+++ src/main/java/com/triprecord/triprecord/location/controller/LocationController.java
@@ -3,6 +3,7 @@ package com.triprecord.triprecord.location.controller;
 import com.triprecord.triprecord.location.PlaceService;
 import com.triprecord.triprecord.location.dto.LocationInfoGetResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,6 @@ public class LocationController {
 
     @GetMapping
     public ResponseEntity<List<LocationInfoGetResponse>> locationInfo(){
-        return ResponseEntity.ok().body(placeService.getLocationInfo());
+        return ResponseEntity.status(HttpStatus.OK).body(placeService.getLocationInfo());
     }
 }
