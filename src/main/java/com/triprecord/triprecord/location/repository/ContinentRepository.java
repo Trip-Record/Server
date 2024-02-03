f6d7ee9996fcb5cdbffe818ea3dcdac5fdc75b96
MinSeoYeon <126096318+gitseoyeon@users.noreply.github.com>
2024-02-04 03:35:36 +0900

    [FEAT] 여행지 장소 조회 API (#60)
    * [FEAT] 여행지 장소 조회 API
    
    * [REFACTOR] API 응답 리턴하는 코드 수정
    
    * [RENAME] 장소 리턴하는 메서드명 수정
    
    * [RENAME] 대륙 정보에 따른 나라 정보 반환하는 메서드명 수정
    
    * [REMOVE] 불필요한 메서드 선언 제거

---
+++ src/main/java/com/triprecord/triprecord/location/repository/ContinentRepository.java
@@ -0,0 +1,11 @@
+package com.triprecord.triprecord.location.repository;
+
+import com.triprecord.triprecord.location.entity.Continent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContinentRepository extends JpaRepository<Continent, Long> {
+}
--- src/main/java/com/triprecord/triprecord/location/repository/PlaceRepository.java
+++ src/main/java/com/triprecord/triprecord/location/repository/PlaceRepository.java
@@ -1,13 +1,16 @@
-package com.triprecord.triprecord.location;
+package com.triprecord.triprecord.location.repository;
 
 
+import com.triprecord.triprecord.location.entity.Country;
 import com.triprecord.triprecord.location.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface PlaceRepository extends JpaRepository<Place, Long> {
     Optional<Place> findByPlaceId(Long placeId);
+    List<Place> findAllByPlaceCountry(Country country);
 }
