20fc9575afd7a83ebc45ceeaa70238cef9c13b51
gitseoyeon <tjdus553926@gmail.com>
2024-02-09 01:28:18 +0900

    [FEAT] 유저의 기록만 불러오는 API

--- src/main/java/com/triprecord/triprecord/user/controller/UserController.java
+++ src/main/java/com/triprecord/triprecord/user/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.triprecord.triprecord.user.controller;
 
 import com.triprecord.triprecord.global.util.ResponseMessage;
+import com.triprecord.triprecord.record.controller.response.RecordPageResponse;
 import com.triprecord.triprecord.user.dto.request.UserCreateRequest;
 import com.triprecord.triprecord.user.dto.response.UserInfoGetResponse;
 import com.triprecord.triprecord.user.dto.request.UserLoginRequest;
 import com.triprecord.triprecord.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -15,8 +18,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.URI;
 
 import java.util.Map;
 
@@ -46,5 +47,10 @@ public class UserController {
     @GetMapping("/informations")
     public ResponseEntity<UserInfoGetResponse> userInfo(Authentication authentication){
         return ResponseEntity.status(HttpStatus.OK).body(userService.getUserInfo(Long.valueOf(authentication.getName())));
+    }
+    @GetMapping("/records")
+    public ResponseEntity<RecordPageResponse> getUserRecords(Authentication authentication, @PageableDefault(size = 5) Pageable pageable){
+        RecordPageResponse response = userService.getUserRecords(Long.valueOf(authentication.getName()), pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
--- src/main/java/com/triprecord/triprecord/user/service/UserService.java
+++ src/main/java/com/triprecord/triprecord/user/service/UserService.java
@@ -5,9 +5,18 @@ import com.triprecord.triprecord.global.config.jwt.JwtProvider;
 import com.triprecord.triprecord.global.config.jwt.UserAuthentication;
 import com.triprecord.triprecord.global.exception.ErrorCode;
 import com.triprecord.triprecord.global.exception.TripRecordException;
+import com.triprecord.triprecord.location.dto.PlaceBasicData;
+import com.triprecord.triprecord.record.controller.response.RecordPageResponse;
+import com.triprecord.triprecord.record.controller.response.RecordResponse;
+import com.triprecord.triprecord.record.dto.RecordImageData;
+import com.triprecord.triprecord.record.entity.Record;
 import com.triprecord.triprecord.record.repository.RecordLikeRepository;
 import com.triprecord.triprecord.record.repository.RecordPlaceRepository;
 import com.triprecord.triprecord.record.repository.RecordRepository;
+import com.triprecord.triprecord.record.service.RecordCommentService;
+import com.triprecord.triprecord.record.service.RecordImageService;
+import com.triprecord.triprecord.record.service.RecordLikeService;
+import com.triprecord.triprecord.record.service.RecordPlaceService;
 import com.triprecord.triprecord.schedule.repository.ScheduleLikeRepository;
 import com.triprecord.triprecord.schedule.repository.ScheduleRepository;
 import com.triprecord.triprecord.user.dto.request.UserCreateRequest;
@@ -15,12 +24,16 @@ import com.triprecord.triprecord.user.dto.request.UserLoginRequest;
 import com.triprecord.triprecord.user.dto.response.UserInfoGetResponse;
 import com.triprecord.triprecord.user.entity.User;
 import com.triprecord.triprecord.user.repository.UserRepository;
+import java.util.ArrayList;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -36,6 +49,10 @@ public class UserService {
     private final ScheduleLikeRepository scheduleLikeRepository;
     private final BasicProfileRepository basicProfileRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RecordLikeService recordLikeService;
+    private final RecordCommentService recordCommentService;
+    private final RecordPlaceService recordPlaceService;
+    private final RecordImageService recordImageService;
 
     @Transactional
     public String signup(UserCreateRequest userCreateRequest) {
@@ -76,6 +93,27 @@ public class UserService {
         Long likeTotal = getLikesCount(user);
 
         return UserInfoGetResponse.of(user, recordTotal, scheduleTotal, placeTotal, likeTotal);
+    }
+
+    public RecordPageResponse getUserRecords(Long userId, Pageable pageable){
+        Page<Record> records = recordRepository.findAllByCreatedUser(userId, pageable);
+        List<RecordResponse> userRecordGetResponse = new ArrayList<>();
+
+        for(Record record : records.getContent()){
+            List<PlaceBasicData> userRecordPlaceData = recordPlaceService.getRecordPlaceBasicData(record);
+            List<RecordImageData> userRecordImageData = recordImageService.findRecordImageData(record);
+
+            Long recordLikeCount = recordLikeService.getRecordLikeCount(record);
+            Long recordCommentCount = recordCommentService.getRecordCommentCount(record);
+
+            userRecordGetResponse.add(RecordResponse.fromRecordData(record, userRecordPlaceData, userRecordImageData, recordLikeCount, recordCommentCount));
+        }
+
+        return RecordPageResponse.builder()
+                .totalPages(records.getTotalPages())
+                .pageNumber(records.getNumber() + 1)
+                .recordList(userRecordGetResponse)
+                .build();
     }
 
     public Long getLikesCount(User user){
