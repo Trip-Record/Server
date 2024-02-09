4fadbd410dac3eea6dfe6114a8787e579c7e602c
gitseoyeon <tjdus553926@gmail.com>
2024-02-09 19:17:01 +0900

    [REFACTOR] 계절별 랭킹 구하는 쿼리문 조건 수정

--- src/main/java/com/triprecord/triprecord/record/repository/RecordPlaceRepository.java
+++ src/main/java/com/triprecord/triprecord/record/repository/RecordPlaceRepository.java
@@ -46,7 +46,7 @@ public interface RecordPlaceRepository extends JpaRepository<RecordPlace, Long> 
     @Query(value = "SELECT r.ranking FROM (" +
             "    SELECT rank() over (order by count(rp.place_id) desc ) as ranking, rp.place_id "
             + "FROM record_place rp LEFT JOIN record re ON rp.record_id = re.record_id WHERE DATE_FORMAT(trip_start_date, '%Y-%m') between :startDate and :middleDate or DATE_FORMAT(trip_start_date, '%Y-%m') = :endDate " +
-            " GROUP BY rp.place_id) r WHERE r.place_id = :pid and r.ranking <= 7", nativeQuery = true)
+            " GROUP BY rp.place_id) r WHERE r.place_id = :pid ", nativeQuery = true)
     Optional<Integer> getRanksBySeason(@Param("startDate") String startDate, @Param("middleDate") String middleDate, @Param("endDate") String endDate, @Param("pid") Long placeId);
 
 }
