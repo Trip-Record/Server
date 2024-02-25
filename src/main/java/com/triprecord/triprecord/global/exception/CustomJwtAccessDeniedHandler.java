57ebfa87c07d6fb5e4ec39cc9e686a38024e9c48
siwon <muncool39@gmail.com>
2024-02-25 21:36:11 +0900

    [FIX] 에러메시지를 반환하도록 수정

--- src/main/java/com/triprecord/triprecord/global/exception/CustomJwtAccessDeniedHandler.java
+++ src/main/java/com/triprecord/triprecord/global/exception/CustomJwtAccessDeniedHandler.java
@@ -1,6 +1,6 @@
 package com.triprecord.triprecord.global.exception;
 
-import jakarta.servlet.ServletException;
+import com.triprecord.triprecord.global.util.ResponseMessage;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.access.AccessDeniedException;
@@ -10,14 +10,17 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
-public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+public class CustomJwtAccessDeniedHandler implements AccessDeniedHandler {
       @Override
-      public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+      public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException)
+              throws IOException {
             setResponse(response);
       }
 
-      private void setResponse(HttpServletResponse response) {
+      private void setResponse(HttpServletResponse response) throws IOException {
             response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.getWriter().write(new ResponseMessage(ErrorCode.FORBIDDEN_ACCESS.getMessage()).toWrite());
       }
 }
--- src/main/java/com/triprecord/triprecord/global/exception/CustomJwtAuthenticationEntryPoint.java
+++ src/main/java/com/triprecord/triprecord/global/exception/CustomJwtAuthenticationEntryPoint.java
@@ -1,7 +1,9 @@
-package com.triprecord.triprecord.global.config.jwt;
+package com.triprecord.triprecord.global.exception;
 
+import com.triprecord.triprecord.global.util.ResponseMessage;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -10,12 +12,16 @@ import org.springframework.stereotype.Component;
 public class CustomJwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
       @Override
-      public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) {
+      public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+              throws IOException {
             setResponse(response);
       }
 
-      private void setResponse(HttpServletResponse response) {
+      private void setResponse(HttpServletResponse response) throws IOException {
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write(new ResponseMessage(ErrorCode.INVALID_TOKEN.getMessage()).toWrite());
       }
 
 }
