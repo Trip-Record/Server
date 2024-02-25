1c9dd59bcc7a981981d6a55a6f7fe3c2ca436a3d
siwon <muncool39@gmail.com>
2024-02-25 21:35:36 +0900

    [ADD] 에러메시지 출력을 위한 메서드 추가

--- src/main/java/com/triprecord/triprecord/global/util/ResponseMessage.java
+++ src/main/java/com/triprecord/triprecord/global/util/ResponseMessage.java
@@ -3,4 +3,9 @@ package com.triprecord.triprecord.global.util;
 public record ResponseMessage(
         String message
 ) {
+    public String toWrite() {
+        return "{" +
+                "\"message\":" + "\"" + message +"\"" +
+                "}";
+    }
 }
