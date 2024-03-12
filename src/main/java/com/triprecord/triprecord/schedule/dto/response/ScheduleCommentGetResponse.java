d8df78c76e2054fe767fe822bf3fe35e380a1053
이채은 <109871579+ChaeAg@users.noreply.github.com>
2024-03-12 13:06:47 +0900

    [FIX] 일정 댓글 목록 조회 API 응답값에 댓글 ID 추가 (#113)

--- src/main/java/com/triprecord/triprecord/schedule/dto/response/ScheduleCommentGetResponse.java
+++ src/main/java/com/triprecord/triprecord/schedule/dto/response/ScheduleCommentGetResponse.java
@@ -7,6 +7,7 @@ import com.triprecord.triprecord.user.dto.response.UserProfile;
 
 public record ScheduleCommentGetResponse(
         UserProfile userProfile,
+        Long commentId,
         String commentContent,
         String commentCreatedTime,
         boolean isUserCreated
@@ -14,6 +15,7 @@ public record ScheduleCommentGetResponse(
     public static ScheduleCommentGetResponse of(ScheduleComment scheduleComment, boolean isUserCreated) {
         return new ScheduleCommentGetResponse(
                 UserProfile.of(scheduleComment.getCommentedUser()),
+                scheduleComment.getScheduleCommentId(),
                 scheduleComment.getScheduleCommentContent(),
                 getDateTime(scheduleComment.getCreatedTime()),
                 isUserCreated
